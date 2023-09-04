744b5d482a2c6ef1d2951927e017c06ca1e81fb8
Ian <128724498+ibtrd@users.noreply.github.com>
2023-09-04 05:07:48 +0200

    Fix c03/ex05

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: ibertran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 21:37:04 by ibertran          #+#    #+#             */
-/*   Updated: 2023/09/04 04:42:43 by ibertran         ###   ########.fr       */
+/*   Updated: 2023/09/04 05:07:33 by ibertran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 	while (dest[d_len])
 		d_len++;
 	s_len = 0;
-	while (src[s_len] && s_len < size)
+	while (src[s_len])
 		s_len++;
 	if (size == 0)
 		return (d_len + s_len);
@@ -30,6 +30,6 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 		dest[d_len + i] = src[i];
 		i++;
 	}
-	src[i] = 0;
+	dest[d_len + i] = 0;
 	return (d_len + s_len);
 }
